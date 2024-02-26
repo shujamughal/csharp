8d87991a6d7abfc65d00c61b846773ae8df2744f
Dr. Shuja ur Rehman Baig <shujamughal@gmail.com>
2024-02-26 09:21:53 +0500

    logging lecture 1 and 2

---
+++ Logging_lec1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+namespace Logging_lec1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string logPath = Path.Combine(Environment.GetFolderPath(
+                            Environment.SpecialFolder.DesktopDirectory), "log.txt");
+            Console.WriteLine($"Writing to: {logPath}");
+            TextWriterTraceListener logFile = new(File.CreateText(logPath));
+            Trace.Listeners.Add(logFile);
+            // text writer is buffered, so this option calls
+            // Flush() on all listeners after writing
+            Trace.AutoFlush = true;
+
+            Debug.WriteLine("Debug says, I am watching!");
+            Trace.WriteLine("Trace says, I am watching!");
+        }
+    }
+}
