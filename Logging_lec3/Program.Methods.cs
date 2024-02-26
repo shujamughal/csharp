4d1814570d14ff005500add3778095d39fc1b30e
Dr. Shuja ur Rehman Baig <shujamughal@gmail.com>
2024-02-26 09:36:25 +0500

    logging lecture 3

---
+++ Logging_lec3/Program.Methods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics; // Trace
+using System.Runtime.CompilerServices; // [Caller...] attributes
+
+namespace Logging_lec1
+{
+    internal partial class Program
+    {
+
+        static void LogSourceDetails( bool condition, 
+            [CallerMemberName] string member = "",
+            [CallerFilePath] string filepath = "",
+            [CallerLineNumber] int line = 0,
+            [CallerArgumentExpression(nameof(condition))] string expression = "")
+        {
+            Trace.WriteLine(string.Format(
+            "[{0}]\n {1} on line {2}. Expression: {3}",
+            filepath, member, line, expression));
+        }
+    }
+}
+
