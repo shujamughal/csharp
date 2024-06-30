09c246b5448da6fafd12647fc6d30105292da888
Dr. Shuja ur Rehman Baig <shujamughal@gmail.com>
2024-06-30 16:58:39 +0500

    adding first lecture on unit testing using NUnit

---
+++ Testing/Unit Testing/MyApplicationTestProject/CalculatorTests.cs
@@ -0,0 +1,35 @@
+﻿using MyApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApplicationTestProject
+{
+	[TestFixture]
+	public class CalculatorTests
+	{
+		private Calculator calculator;
+
+		[SetUp]
+		public void Setup()
+		{
+			calculator = new Calculator();
+		}
+
+		[Test]
+		public void Add_ReturnsCorrectSum()
+		{
+			// Arrange
+			int a = 2;
+			int b = 3;
+
+			// Act
+			int result = calculator.Add(a, b);
+
+			// Assert
+			Assert.AreEqual(5, result);
+		}
+	}
+}
